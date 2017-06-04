97e8b47c7fa7748c5fd25f523bb81a7813d957d8
evisagaj <eviaga12@htl-shkoder.com>
2017-06-04 17:21:37 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,74 @@
+<?php
+if(!isset($_SESSION)) {
+		session_start();
+	}	
+	var_dump($_SESSION);
+	if(isset($_SESSION["userID"])){
+		header("Location: home.php");
+	}
+	require('dbconnect.php');
+	?>
+<!DOCTYPE html>
+<?php
+
+	$pdo = new PDO('mysql:host=localhost;port=3306;dbname=webshop', 'root', '');
+	try {
+	$pdo = new PDO('mysql:host=localhost;dbname=webshop;port=3306', 'root', '');
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	} catch(PDOException $e) {
+		echo $e->getMessage();
+		exit();
+	}
+?>
+<html>
+ <head>
+<link rel="stylesheet" href="style.css" type="text/css" />	
+    <title> Business Bridge </title>
+ </head>
+ 
+<body>
+
+<!-- webpage content goes here in the body -->
+        <?php require('menu.php'); ?>
+		<div id="content">
+			<h2> Logge Dich ein </h2>
+			
+		<?php
+		if(isset($_GET['login'])){
+			//Dateneingabe
+			$email = $_POST['email'];
+			$password = $_POST['password'];
+
+			$statement = $pdo->prepare("SELECT * FROM user1 WHERE email = ?");
+			$statement ->bindParam(1,$email);
+			$result = $statement->execute();
+			 
+			$res = $statement->fetch();
+			$data = $res[1] ."\n" . $res[2] . "\n";
+			
+			if($res[1] == $email && $res[2] == $password){
+				
+			
+			
+			$_SESSION['email'] = $_POST['email'];
+			$_SESSION['vname'] = $_POST['vname'];
+			$_SESSION['userlevel'] = $res[6];
+			$_SESSION['userID'] = $res[0];
+			
+			header('Location: home.php');
+			}
+		}
+		?>
+				<form action="?login=1" method="post">
+					E-Mail: <br> <input type="email" size="40" maxlength="250" name="email"><br><br>
+					Dein Passwort:<br> <input type="password" size="40"  maxlength="250" name="password"><br>
+					<input type="submit" value="Send">
+				</form>
+ 
+		</div>
+	</div>
+</body>
+</html>
+<?php
+$pdo->connection = null;
+?>
---
+++ register.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<?php
+if(!isset($_SESSION)) {
+		session_start();
+	}
+ require('dbconnect.php');
+?>
+<html>
+ <head>
+<link rel="stylesheet" href="style.css" type="text/css" />	
+    <title> Business Bridge Startseite </title>
+ </head>
+<body>
+        <?php require('menu.php'); ?>
+		<div id="content">
+			<h2> Register as a Costumer </h2>
+			<?php
+			if(isset($_GET['register']))
+			{
+				$email = $_POST['email'];
+				$password = $_POST['password'];
+				$vname = $_POST['vname'];
+				$fname = $_POST['fname'];
+				$phone = $_POST['phone'];
+				
+				$statement = $pdo->prepare("SELECT * FROM user1 WHERE email = ?");
+				$statement ->bindParam(1,$email);
+				
+				$result = $statement->execute();
+				if($statement->rowCount() > 0 ) {
+                    echo 'Diese E-Mail Addresse ist besetzt. <br>';
+					?>
+					 <a href="login.php"> Logge Dich ein! </a> 
+			<?php 
+			    } else{
+				    $insert = "INSERT INTO user1 (email,password,vname,fname,phone, userlevel) VALUES (?,?,?,?,?,1)";
+     				$prepStmt = $pdo->prepare($insert);
+					//$prepStmt ->bindParam(1, $userID);
+					$prepStmt ->bindParam(1, $email);	
+					$prepStmt ->bindParam(2, $password);
+					$prepStmt ->bindParam(3, $vname);
+					$prepStmt ->bindParam(4, $fname);
+					$prepStmt ->bindParam(5, $phone);
+					$prepStmt -> execute();
+					
+					echo "<h2> Willkommen, $vname! Du kannst dich jetzt  einloggen und Produkten kaufen und verkaufen. </h2>";
+				}
+			}
+			else{
+			?>
+				<form action="?register=1" method="post">
+					E-Mail: <br> <input type="email" size="40" maxlength="250" name="email"><br><br>
+					Passwort:<br> <input type="password" size="40"  maxlength="250" name="password"><br>
+					Vorname: <br> <input type="vname" size="40" maxlength="250" name="vname"><br><br>
+					Nachname: <br> <input type="fname" size="40" maxlength="250" name="fname"><br><br>
+					Telefonnummer: <br> <input type="phone" size="40" maxlength="250" name="phone"><br><br>
+					<input type="submit" value="Submit">
+				</form>
+		</div>
+	</div>
+</body>
+</html>
+<?php
+			}
+$pdo = null;
+?>
